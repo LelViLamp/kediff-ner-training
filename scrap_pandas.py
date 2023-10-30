4483d4ab12b1000c047334324757685379414391
Daniel Göller <61782325+LelViLamp@users.noreply.github.com>
2023-10-30 13:58:51 +0100

    remove confusing parts

--- scrap_pandas.py
+++ scrap_pandas.py
@@ -36,64 +36,6 @@ def print_aligned(words: list, tags: list):
     print(line2)
 
 
-# a method for adding labels to the tokenised input
-def find_word_ids_for_span(tokenised_input: BatchEncoding, span) -> Tuple[int, int]:
-    offsets = tokenised_input.encodings[0].offsets
-    word_ids = tokenised_input.word_ids()
-    start: int = span[0]
-    end: int = span[1]
-
-    start_word_id: int = 0
-    end_word_id: int = 0
-
-    while start_word_id == 0:
-        for idx, tuple in enumerate(offsets):
-            ostart = tuple[0]
-            if start <= ostart:
-                start_word_id = word_ids[idx]
-                break
-    while end_word_id == 0:
-        for idx, tuple in enumerate(offsets):
-            oend = tuple[1]
-            if end <= oend:
-                end_word_id = word_ids[idx]
-                break
-
-    return (start_word_id, end_word_id)
-
-
-def map_annotations(tokenised_input: BatchEncoding, annotations):
-    spans = []
-    for annotation in annotations:
-        word_span = find_word_ids_for_span(tokenised_input, annotation)
-        spans.append(word_span)
-    
-    return spans
-
-
-def align_labels_with_tokens(labels, word_ids):
-    new_labels = []
-    current_word = None
-    for word_id in word_ids:
-        if word_id != current_word:
-            # Start of a new word!
-            current_word = word_id
-            label = -100 if word_id is None else labels[word_id]
-            new_labels.append(label)
-        elif word_id is None:
-            # Special token
-            new_labels.append(-100)
-        else:
-            # Same word as previous token
-            label = labels[word_id]
-            # If the label is B-XXX we change it to I-XXX
-            if label % 2 == 1:
-                label += 1
-            new_labels.append(label)
-
-    return new_labels
-
-
 #%% prepare label-specific datasets
 present_labels = (
     annotations
