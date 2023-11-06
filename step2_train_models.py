29cd84232a5997ec7fea37685900e21eee1db540
Daniel Göller <61782325+LelViLamp@users.noreply.github.com>
2023-11-06 14:18:46 +0100

    Update step2_train_models.py

--- step2_train_models.py
+++ step2_train_models.py
@@ -10,8 +10,8 @@ print(bilus_hug)
 print(bilus_hug.features)
 
 # %% split dataset into train test val
-train_testvalid = bilus_hug.train_test_split(test_size = 0.2)
-test_valid = train_testvalid['test'].train_test_split(test_size = 0.5)
+train_testvalid = bilus_hug.train_test_split(test_size = 0.2, seed = 42)
+test_valid = train_testvalid['test'].train_test_split(test_size = 0.5, seed = 42)
 # gather everyone if you want to have a single DatasetDict
 bilus_hug = DatasetDict({
     'train': train_testvalid['train'],
@@ -27,17 +27,22 @@ tokeniser: BertTokenizerFast = AutoTokenizer.from_pretrained(checkpoint)
 print(f"Is '{checkpoint}' a fast tokeniser?", tokeniser.is_fast)
 
 
-def batch_tokenise(batch):
+def batch_tokenise_and_embed(batch):
+    # tokenise
     tokenised_inputs = tokeniser(batch['Text'], truncation=True)
+    # # align annotation with added [CLS] and [SEP]
+    # for bilu_column in ['EVENT-BILUs','LOC-BILUs','MISC-BILUs','ORG-BILUs','PER-BILUs','TIME-BILUs']:
+    #     all_labels = batch[bilu_column]
+    #     new_labels = [[-100, *labels, -100] for labels in all_labels]
+    #     tokenised_inputs[bilu_column] = new_labels
     return tokenised_inputs
 
 
-bilus_hug_tokenised = bilus_hug.map(batch_tokenise, batched=True)
+bilus_hug_tokenised = bilus_hug.map(batch_tokenise_and_embed, batched=True)
 print(bilus_hug_tokenised)
 
 # %% get a sample
-samples = bilus_hug_tokenised["train"].shuffle(seed=42).select(range(20))
-sample = samples[2]
+sample = bilus_hug_tokenised["train"][1]
 sample
 
 # %% debug
